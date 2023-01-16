ac67a726e8fb46eb7965e98adafd32ce3d13a365
Rakesh1545 <122012349+Rakesh1545@users.noreply.github.com>
2023-01-16 15:51:54 +0530

    Add files via upload

---
+++ Untitled5.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+list1 = [2,4,6,8,10]
+square = [i**2 for i in list1]
+print('square of the numbers are:',square)
+
+
+# In[3]:
+
+
+cube = [i**3 for i in list1]
+print('cube of the numbers are:',cube)
+
+
+# In[ ]:
+
+
+
+
